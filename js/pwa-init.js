6557ba3956c88ca75ff112ba420413b8b01ee8fb
Kirill Voronin <kirik4444@yandex.ru>
2026-06-29 09:55:01 +0500

    fix

--- js/pwa-init.js
+++ js/pwa-init.js
@@ -55,17 +55,17 @@ function updatePWAManifest(opts) {
         background_color: bgColor,
         icons: [
 {
-                src: "./icons/android-chrome-192x192.png",
+                src: "/currency-converter/icons/android-chrome-192x192.png",
                 sizes: "192x192",
                 type: "image/png"
             },
             {
-                src: "./icons/android-chrome-512x512.png",
+                src: "/currency-converter/icons/android-chrome-512x512.png",
                 sizes: "512x512",
                 type: "image/png"
             },
             {
-                src: "./icons/android-chrome-512x512.png",
+                src: "/currency-converter/icons/android-chrome-512x512.png",
                 sizes: "512x512",
                 type: "image/png",
                 purpose: "maskable"
