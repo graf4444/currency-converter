e1727a1903b21778518a3e765d3522676e71553c
Kirill Voronin <kirik4444@yandex.ru>
2026-08-03 21:08:10 +0500

    update vers

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE_NAME on every release that changes any file in ASSETS
-const CACHE_NAME = 'currency-converter-v8';
+const CACHE_NAME = 'currency-converter-v9';
 const ASSETS = [
   './',
   './index.html',
